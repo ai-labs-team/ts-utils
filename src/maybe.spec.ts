58ea073cf85bf1715b926d848bae3077a6c91497
Nate Abele <nate@radify.io>
2019-12-10 16:41:05 -0500

    Implement Maybe.all()
    Better type parameter names for Maybe
    
    Maybe specs

---
+++ src/maybe.spec.ts
@@ -0,0 +1,83 @@
+import { expect } from 'chai';
+import { pipe } from 'ramda';
+import Maybe from './maybe';
+
+describe('Maybe', () => {
+
+  describe('all', () => {
+    it('transforms a list of non-empty maybes to a maybe of a list', () => {
+      expect(Maybe.all([1, 2, 3, 4, 5].map(Maybe.of))).to.deep.equal(Maybe.of([1, 2, 3, 4, 5]));
+    });
+
+    it('returns Nothing when a list item is Nothing', () => {
+      expect(Maybe.all([1, 2, 3, 4, 5].map(Maybe.of).concat([Maybe.empty]))).to.deep.equal(Maybe.empty);
+    });
+  });
+
+  describe('defaultTo', () => {
+    it('uses default values when empty', () => {
+      expect((new Maybe<number>(null)).defaultTo(1)).to.equal(1);
+      expect(pipe(Maybe.of, Maybe.defaultTo(1))(null)).to.equal(1);
+    });
+  });
+
+  describe('defaultToLazy', () => {
+    let evaluated: boolean;
+
+    beforeEach(() => evaluated = false);
+
+    it('does not evaluate when present', () => {
+      expect(Maybe.of(41).defaultToLazy(() => {
+        evaluated = true;
+        return 42;
+      })).to.equal(41);
+
+      expect(evaluated).to.equal(false);
+    });
+
+    it('evaluates when empty', () => {
+      expect(Maybe.emptyOf<number>().defaultToLazy(() => {
+        evaluated = true;
+        return 42;
+      })).to.equal(42);
+
+      expect(evaluated).to.equal(true);
+    });
+  });
+
+  describe('map', () => {
+    /**
+     * @link https://github.com/sanctuary-js/sanctuary-maybe#maybefantasy-landmap--maybeaa-b---maybeb
+     */
+    describe(`sanctuary-maybe spec`, () => {
+      it('mimics spec 1', () => {
+        expect(Maybe.empty.map(Math.sqrt)).to.deep.eq(Maybe.empty);
+      });
+
+      it('mimics spec 2', () => {
+        expect(Maybe.of(9).map(Math.sqrt)).to.deep.eq(Maybe.of(3));
+      });
+    });
+  });
+
+  describe('chain', () => {
+    /**
+     * @link https://github.com/sanctuary-js/sanctuary-maybe#maybefantasy-landchain--maybeaa-maybeb---maybeb
+     */
+    describe(`sanctuary-maybe spec`, () => {
+      const ofHead = <Val>(xs: Val[]) => xs.length === 0 ? Maybe.empty : Maybe.of(xs[0]);
+
+      it('mimics spec 1', () => {
+        expect(Maybe.empty.chain(ofHead)).to.deep.eq(Maybe.empty);
+      });
+
+      it('mimics spec 2', () => {
+        expect(Maybe.of([]).chain(ofHead)).to.deep.eq(Maybe.empty);
+      });
+
+      it('mimics spec 3', () => {
+        expect(Maybe.of(['foo', 'bar', 'baz']).chain(ofHead)).to.deep.eq(Maybe.of('foo'));
+      });
+    });
+  });
+});
--- src/maybe.ts
+++ src/maybe.ts
@@ -1,32 +1,42 @@
-import { any, anyPass, curry, ifElse, isEmpty, isNil, map, pipe } from 'ramda';
+import { any, complement, anyPass, curry, ifElse, isEmpty, isNil, map, pipe, all } from 'ramda';
 
-export default class Maybe<T> {
+export default class Maybe<Val> {
 
   public static map = curry(<T, U>(fn: (val: T) => U, maybe: Maybe<T>) => maybe.map(fn));
 
   public static empty = Maybe.of<any>(null);
 
-  public static defaultToLazy = curry(<T>(fn: () => T, maybe: Maybe<T>) => maybe.defaultToLazy(fn));
+  public static defaultToLazy = curry(<Val>(fn: () => Val, maybe: Maybe<Val>) => maybe.defaultToLazy(fn));
 
-  public static defaultTo = curry(<T>(val: T, maybe: Maybe<T>) => maybe.defaultTo(val));
+  public static defaultTo = curry(<Val>(val: Val, maybe: Maybe<Val>) => maybe.defaultTo(val));
 
-  public static orLazy = curry(<T>(fn: () => Maybe<T>, maybe: Maybe<T>) => maybe.orLazy(fn));
+  public static orLazy = curry(<Val>(fn: () => Maybe<Val>, maybe: Maybe<Val>) => maybe.orLazy(fn));
 
-  public static or = curry(<T>(val: Maybe<T>, maybe: Maybe<T>) => maybe.or(val));
+  public static or = curry(<Val>(val: Maybe<Val>, maybe: Maybe<Val>) => maybe.or(val));
 
-  public static isNothing = <T>(maybe: Maybe<T>) => maybe.isNothing();
+  public static isNothing = <Val>(maybe: Maybe<Val>) => maybe.isNothing();
 
-  public static value = <T>(maybe: Maybe<T>) => maybe.value();
+  public static value = <Val>(maybe: Maybe<Val>) => maybe.value();
 
-  public static of<T>(val: T | null | undefined): Maybe<T> {
+  public static of<Val>(val: Val | null | undefined): Maybe<Val> {
     return new Maybe(val);
+  }
+
+  /**
+   * Maps a `Array<Maybe<Val>>` into a `Maybe<Array<Val>>` _only_ if none of the array elements
+   * is `Nothing`.
+   */
+  public static all = <Val>(list: Maybe<Val>[]): Maybe<Val[]> => {
+    return all(complement(Maybe.isNothing), list)
+      ? Maybe.of(list.map(Maybe.value))
+      : Maybe.empty;
   }
 
   /**
    * Converts a list of `Maybe` to a `Maybe` list, which is `Nothing` unless _all_
    * items in the list have a value.
    */
-  public static toList: <T>(maybe: Array<Maybe<T>>) => Maybe<T[]> = ifElse(
+  public static toList: <Val>(maybe: Array<Maybe<Val>>) => Maybe<Val[]> = ifElse(
     anyPass([isNil, isEmpty, any(Maybe.isNothing)]),
     () => Maybe.empty,
     pipe(map(Maybe.value), Maybe.of),
@@ -40,15 +50,15 @@ export default class Maybe<T> {
    * Maybe.emptyOf<string>(): Maybe<string>
    * ```
    */
-  public static emptyOf = <T>() => Maybe.empty as Maybe<T>;
+  public static emptyOf = <Val>() => Maybe.empty as Maybe<Val>;
 
-  private val!: T;
+  private val!: Val;
 
-  constructor(val: T | null | undefined) {
+  constructor(val: Val | null | undefined) {
     Object.freeze(Object.assign(this, { val }));
   }
 
-  public map<U>(fn: (val: T) => U): Maybe<U> {
+  public map<NewVal>(fn: (val: Val) => NewVal): Maybe<NewVal> {
     return this.isNothing() ? Maybe.empty : Maybe.of(fn(this.val));
   }
 
@@ -56,27 +66,27 @@ export default class Maybe<T> {
     return (this.val === null || this.val === undefined);
   }
 
-  public value(): T | null {
+  public value(): Val | null {
     return this.isNothing() ? null : this.val;
   }
 
-  public chain<U>(fn: (val: T) => Maybe<U>): Maybe<U> {
-    return this.isNothing() ? this as any as Maybe<U> : fn(this.val);
+  public chain<NewVal>(fn: (val: Val) => Maybe<NewVal>): Maybe<NewVal> {
+    return this.isNothing() ? this as unknown as Maybe<NewVal> : fn(this.val);
   }
 
-  public defaultTo(val: T): T {
+  public defaultTo(val: Val): Val {
     return this.isNothing() ? val : this.val;
   }
 
-  public defaultToLazy(fn: () => T): T {
+  public defaultToLazy(fn: () => Val): Val {
     return this.isNothing() ? fn() : this.val;
   }
 
-  public or(alt: Maybe<T>): Maybe<T> {
+  public or(alt: Maybe<Val>): Maybe<Val> {
     return this.isNothing() ? alt : this;
   }
 
-  public orLazy(fn: () => Maybe<T>): Maybe<T> {
+  public orLazy(fn: () => Maybe<Val>): Maybe<Val> {
     return this.isNothing() ? fn() : this;
   }
 }
